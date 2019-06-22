ac7cf152bd31e2e9375c9b10e8daa1ca1615fb62
dx <505707606@qq.com>
2019-06-22 13:15:19 +0800

    eureka服务搭建完毕

---
+++ hello-spring-cloud-eureka/src/main/java/dx/hello/spring/cloud/eureka/EurekaApplication.java
@@ -0,0 +1,19 @@
+package dx.hello.spring.cloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @description: eureka的启动类
+ * @author: zhang.da.xin
+ * @create: 2019-06-22 12:15
+ **/
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
