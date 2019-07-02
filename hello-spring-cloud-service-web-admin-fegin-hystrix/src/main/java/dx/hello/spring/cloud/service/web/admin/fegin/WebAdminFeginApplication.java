c72aed2bd9d4fe327e6599b9cda24d34c8d52fd5
dx <505707606@qq.com>
2019-07-02 22:34:14 +0800

    增加依赖以及注解@EnableHystrixDashboard

--- hello-spring-cloud-service-web-admin-fegin-hystrix/src/main/java/dx/hello/spring/cloud/service/web/admin/fegin/WebAdminFeginApplication.java
+++ hello-spring-cloud-service-web-admin-fegin-hystrix/src/main/java/dx/hello/spring/cloud/service/web/admin/fegin/WebAdminFeginApplication.java
@@ -3,6 +3,7 @@ package dx.hello.spring.cloud.service.web.admin.fegin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableHystrixDashboard
 public class WebAdminFeginApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebAdminFeginApplication.class,args);
